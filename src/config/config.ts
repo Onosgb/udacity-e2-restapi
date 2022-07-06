5b5ebf9d8f537567b0f225a4545917b18b85bd74
Onosgb <onosgb@gmail.com>
2022-07-06 22:35:20 +0100

    added the configuration details for deployment

---
+++ src/config/config.ts
@@ -0,0 +1,23 @@
+// require("dotenv").config();
+export const config = {
+  dev: {
+    username: "udagramonosgbdev",
+    password: "udagramonosgbdev",
+    database: "udagramonosgbdev",
+    host: "udagramonosgbdev.cwiqfnlww4pb.us-east-1.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: "us-east-1",
+    aws_profile: "default",
+    aws_media_bucket: "udagram-onosgb-dev",
+  },
+  jwt: {
+    secret: "",
+  },
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
